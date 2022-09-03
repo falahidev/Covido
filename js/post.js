38bce22219fde65703431fd6315cc9b7b7275544
Mohammad <falahidev.info@gmail.com>
2022-09-03 17:34:44 +0430

    update and change show post page structure and script files

--- js/post.js
+++
@@ -1,8 +0,0 @@
-const linkBox = document.querySelector('.link-box');
-const link = document.querySelector('.link');
-
-linkBox.addEventListener('click', () => {
-   navigator.clipboard.writeText(link.value);
-
-   showSnack('کپی شد');
-}, true);
